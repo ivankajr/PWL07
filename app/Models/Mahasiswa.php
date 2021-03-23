113f7c0c849dfb4d90628254ef37bb05d504b718
ivankajr <ivnka24@gmail.com>
2021-03-23 11:21:42 +0700

    mengisi model mahasiswa

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model; //model Eloquent
 
-class Mahasiswa extends Model
+class Mahasiswa extends Model //definisi model
 {
-    use HasFactory;
-}
+    protected $table="mahasiswa"; // Eloquent akan membuat model mahasiswa menyimpan record di table mahasiswas
+    public $timestamps=false;
+    protected $primaryKey = 'nim'; // Memanggil isi DB dengan Primarykey
+    
+    protected $fillable = [
+        'nim',
+        'nama',
+        'kelas',
+        'jurusan',
+        'no_hp',
+    ];
+};
