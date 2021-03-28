e5d100c1a2eb501737b535be25129493880f1617
ivankajr <ivnka24@gmail.com>
2021-03-28 14:33:17 +0700

    add tanggal lahir and email in index

--- resources/views/mahasiswas/index.blade.php
+++ resources/views/mahasiswas/index.blade.php
@@ -22,25 +22,41 @@
     <th>Kelas</th>
     <th>Jurusan</th>
     <th>No_Handphone</th>
+    <th>Email</th>
+    <th>Tanggal_Lahir</th>
     <th width="280px">Action</th>
 </tr>
-@foreach ($mahasiswas as $Mahasiswa)
-<tr>
-    <td>{{ $Mahasiswa->nim }}</td>
-    <td>{{ $Mahasiswa->nama }}</td>
-    <td>{{ $Mahasiswa->kelas }}</td>
-    <td>{{ $Mahasiswa->jurusan }}</td>
-    <td>{{ $Mahasiswa->no_hp }}</td>
-    <td>
-        <form action="{{ route('mahasiswa.destroy', $Mahasiswa->nim) }}" method="POST">
-            <a class="btn btn-info" href="{{ route('mahasiswa.show',$Mahasiswa->nim) }}">Show</a>
-            <a class="btn btn-primary" href="{{ route('mahasiswa.edit',$Mahasiswa->nim) }}">Edit</a>
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger">Delete</button>
-        </form>
-    </td>
-</tr>
-@endforeach
-</table>
+    @if (count($posts) > 0)
+            @foreach ($posts as $Mahasiswa)
+            <tr>
+                <td>{{ $Mahasiswa->nim }}</td>
+                <td>{{ $Mahasiswa->nama }}</td>
+                <td>{{ $Mahasiswa->kelas }}</td>
+                <td>{{ $Mahasiswa->jurusan }}</td>
+                <td>{{ $Mahasiswa->no_handphone }}</td>
+                <td>{{ $Mahasiswa->email }}</td>
+                <td>{{ $Mahasiswa->tanggal_lahir }}</td>
+                <td>
+                <form action="{{ route('mahasiswa.destroy',$Mahasiswa->nim) }}" method="POST">
+                    <a class="btn btn-info" href="{{ route('mahasiswa.show',$Mahasiswa->nim) }}">Show</a>
+                    <a class="btn btn-primary" href="{{ route('mahasiswa.edit',$Mahasiswa->nim) }}">Edit</a>
+                        @csrf 
+                        @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+                </td>
+            </tr>
+            @endforeach
+        @else 
+            <div class="float-left my-2">
+                <h5 class="text-danger">Mahasiswa Not Found !</h4>
+            </div>
+            <div class="float-right my-2">
+                <a class="btn btn-success mt-3" href="{{ route('mahasiswa.index') }}">Kembali</a> 
+            </div>
+        @endif
+    </table>
+    <div class="d-flex float-none">
+        {{$posts->links('pagination::bootstrap-4')}}
+    </div>
 @endsection
