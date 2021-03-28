53a82b430edfa0f81362ef8391c262d48c6cb301
ivankajr <ivnka24@gmail.com>
2021-03-28 14:36:17 +0700

    add email and tanggal lahir in create blade

--- resources/views/mahasiswas/create.blade.php
+++ resources/views/mahasiswas/create.blade.php
@@ -35,7 +35,15 @@
         </div>
         <div class="form-group">
             <label for="no_hp">No_Handphone</label>
-            <input type="nno_hp" name="no_hp" class="form-control" id="no_hp" aria-describedby="no_hp" >
+            <input type="no_hp" name="no_hp" class="form-control" id="no_hp" aria-describedby="no_hp" >
+        </div>
+        <div class="form-group">
+            <label for="email">email</label>
+            <input type="email" name="email" class="form-control" id="email" aria-describedby="email" >
+        </div>
+        <div class="form-group">
+            <label for="tanggal_lahir">tanggal_lahir</label>
+            <input type="tanggal_lahir" name="tanggal_lahir" class="form-control" id="tanggal_lahir" aria-describedby="tanggal_lahir" >
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
